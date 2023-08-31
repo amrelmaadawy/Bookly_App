90930b14362748da89aca583c32d079a65160a09
amrelmaadawy <amrelmaadawy905@gmail.com>
2023-08-31 23:26:36 +0300

    cached network image

--- lib/Features/home/persentaion/views/widgets/customBookItemListView.dart
+++ lib/Features/home/persentaion/views/widgets/customBookItemListView.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class customItemListView extends StatelessWidget {
   const customItemListView({Key? key, required this.imageUrl})
@@ -6,14 +8,13 @@ class customItemListView extends StatelessWidget {
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image:  DecorationImage(
-              image: NetworkImage(imageUrl), fit: BoxFit.fill),
-        ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: CachedNetworkImage(imageUrl: imageUrl,fit: BoxFit.fill,
+        errorWidget: (context, url, error) =>  const Icon(Icons.error),
+        )
       ),
     );
   }
